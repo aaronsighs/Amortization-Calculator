8367888547a0b58a96decb4b3b5904364332a1fb
admin <aaronsyers@gmail.com>
2024-03-23 13:23:50 -0500

    added stats section to layout

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+
+termToggleBtn = document.getElementById("btn-toggle-term-table")
+
+
+document.getElementById("monthToggleButton").onclick = toggleTableView
+document.getElementById("yearToggleButton").onclick = toggleTableView
+
+function toggleTableView(){
+    monthTableView = document.getElementsByClassName("tsc-two")[0]
+    yearTableView = document.getElementsByClassName("tsc-one")[0]
+
+    if (monthTableView.classList.contains("hidden")){
+        monthTableView.classList.remove("hidden")
+        yearTableView.classList.add("hidden")
+    }else{
+        yearTableView.classList.remove("hidden")
+        monthTableView.classList.add("hidden")
+    }
+
+}
+
+
+
+
+
+
+
+
