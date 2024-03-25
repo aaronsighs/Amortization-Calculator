8770ac76b7f1ca7439f1917afd62f9696ae14e3e
admin <aaronsyers@gmail.com>
2024-03-24 19:26:37 -0500

    finished month table generation

--- script.js
+++ script.js
@@ -36,10 +36,67 @@ function calculate(){
     if (checkInput().length!=0){return 0}
     monthlyPayment = calculateMonthlyPayment(getLoanPrincipal(),getInterestRate(),getLoanTerm())
     document.getElementById("monthly-payment").innerText = "Monthly Payment: $" + monthlyPayment.toFixed(2)
+    clearMonthTable()
+    addAllRowsToMonthTable(getLoanPrincipal(),getInterestRate(),getLoanTerm())
+}
+
+function clearMonthTable(){
+    monthTable = document.getElementById("output-table-months")
+    while (monthTable.childElementCount!=1) {
+        monthTable.removeChild(monthTable.lastChild);
+    }
+}
+
+function addAllRowsToMonthTable(principal,interestRate,term_months){
+
+    monthlyInterestRate = interestRate / 100  / 12
+    monthlyPayment = calculateMonthlyPayment(getLoanPrincipal(),getInterestRate(),getLoanTerm())
+    
+    balance = principal
+    count = 1
+    while(balance>0){
+
+        principalInterest = (monthlyInterestRate * balance)
+        principalPaid = monthlyPayment - principalInterest
+        balance -= principalPaid
+        addRowToMonthTable([count,principalInterest.toFixed(2),principalPaid.toFixed(2),balance.toFixed(2)])
+        if (count % 12 == 0 && balance>0){
+            addRowToMonthTable(["End of Year "+count/12],rowClass="",itemClass="apple",span=1)
+        }
+        count+=1
+        
+        
+
+        
+    }
+
+
 }
 
 
+function addRowToMonthTable(data,rowClass="",itemClass="",span=0){
+    monthTable = document.getElementById("output-table-months")
 
+    tableRow = document.createElement("tr")
+    rowClass ? tableRow.classList.add(rowClass):""
+    data.forEach((element) => {
+        tableItem = document.createElement("td")
+        tableItem.innerText = element
+
+        if (itemClass){tableItem.classList.add(itemClass)}
+        if (span){tableItem.colSpan=10}
+
+        tableRow.appendChild(tableItem)
+    })
+    monthTable.appendChild(tableRow)
+}
+
+function UpdatePieChart(percentInterest){
+    piechart = document.getElementsByClassName("piechart")[0]
+    background ="conic-gradient(rgba(26, 25, 25, 0.621)"+percentInterest/100*360+"deg,rgba(24, 203, 134, 0.962) 0)a"
+    piechart.style.backgroundImage = background =background
+    piechart.classList.remove("hidden")
+}
 
 function checkInput(){
     checks = ""
